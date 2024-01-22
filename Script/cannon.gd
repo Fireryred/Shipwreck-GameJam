bcdf0eb2af1174be41178131868d496ea1a9c61f
Fireryred <gershomgruta06@gmail.com>
2024-01-22 17:04:54 +0800

    add on hold left click

--- Script/cannon.gd
+++ Script/cannon.gd
@@ -4,13 +4,22 @@ extends Node2D
 
 var cannon_ball_scene := preload("res://Scenes/cannonball.tscn")
 
+var is_left_click_hold := false
+
 
 func _process(delta):
 	look_at(get_global_mouse_position())
+	if (is_left_click_hold):
+		_shoot()
 
 func _unhandled_input(event):
 	if event.is_action_pressed("left_click"):
-		var bullet := cannon_ball_scene.instantiate()
-		bullet.position = shoot_position.global_position
-		bullet.direction = global_position.direction_to(get_global_mouse_position())
-		add_child(bullet)
+		is_left_click_hold = true
+	elif event.is_action_released("left_click"):
+		is_left_click_hold = false
+
+func _shoot():
+	var bullet := cannon_ball_scene.instantiate()
+	bullet.position = shoot_position.global_position
+	bullet.direction = global_position.direction_to(get_global_mouse_position())
+	add_child(bullet)
