f5d234b846a90ec4196ae6f87dd8c1da99037aaf
Fireryred <gershomgruta06@gmail.com>
2024-01-22 17:47:42 +0800

    added firerate for cannon

--- Script/cannon.gd
+++ Script/cannon.gd
@@ -1,15 +1,16 @@
 extends Node2D
 
-@onready var shoot_position = $ShootPosition
+@onready var shoot_position := $ShootPosition
+@export var firerate := 1.0
 
 var cannon_ball_scene := preload("res://Scenes/cannonball.tscn")
 
 var is_left_click_hold := false
-
+var is_delay_over := true
 
 func _process(delta):
 	look_at(get_global_mouse_position())
-	if (is_left_click_hold):
+	if (is_left_click_hold && is_delay_over):
 		_shoot()
 
 func _unhandled_input(event):
@@ -19,7 +20,16 @@ func _unhandled_input(event):
 		is_left_click_hold = false
 
 func _shoot():
+	_add_bullet()
+	_add_delay()
+
+func _add_bullet():
 	var bullet := cannon_ball_scene.instantiate()
 	bullet.position = shoot_position.global_position
 	bullet.direction = global_position.direction_to(get_global_mouse_position())
 	add_child(bullet)
+
+func _add_delay():
+	is_delay_over = false
+	await get_tree().create_timer(firerate).timeout
+	is_delay_over = true
