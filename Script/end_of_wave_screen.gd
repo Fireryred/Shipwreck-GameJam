961d371b19929beeff84dcc2d15dd36e9d05e7ef
Fireryred <gershomgruta06@gmail.com>
2024-01-26 21:03:44 +0800

    fix card selection

--- Script/end_of_wave_screen.gd
+++ Script/end_of_wave_screen.gd
@@ -2,4 +2,5 @@ extends Control
 
 func _unhandled_input(event):
 	if event.is_action_pressed("left_click") and visible:
+		get_tree().paused = false
 		get_tree().change_scene_to_file("res://Scenes/card_select.tscn")
